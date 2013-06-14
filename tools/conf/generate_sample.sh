cf84f2bf8a12b82ff91b5918553c61c3c2763787
Anita Kuno <anita.kuno@enovance.com>
2013-06-14 16:54:03 -0400

    Add conf file generator.
    Add oslo generator.py and generate_sample.sh similar to nova.
    These files provide generator functionality for creating a conf file.
    
    Fixes: bug #1191032
    Change-Id: I5a239e41f822e2f51c95d7a44e8d62c38c62c8ac

---
+++ tools/conf/generate_sample.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2012 SINA Corporation
+# All Rights Reserved.
+# Author: Zhongyue Luo <lzyeval@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+FILES=$(find ironic -type f -name "*.py" ! -path "ironic/tests/*" \
+        ! -path "ironic/nova/*" -exec grep -l "Opt(" {} + | sort -u)
+
+export EVENTLET_NO_GREENDNS=yes
+
+MODULEPATH=$(dirname "$0")/../../ironic/openstack/common/config/generator.py
+OUTPUTPATH=etc/ironic/ironic.conf.sample
+PYTHONPATH=./:${PYTHONPATH} python $MODULEPATH $FILES > $OUTPUTPATH
