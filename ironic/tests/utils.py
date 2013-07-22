4614fb4d57617cdcf342602ea66c37396b2ed0a6
Yuriy Zveryanskyy <yzveryanskyy@mirantis.com>
2013-07-22 15:30:37 +0300

    Add common code from Oslo for db migrations test
    Moved common code for db migrations test from Oslo.
    
    partially implements blueprint db-migration-tests
    
    Change-Id: Icf8c27a40ccc0114e6001cfa64444ca1cdb132fa

---
+++ ironic/tests/utils.py
@@ -0,0 +1,80 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2010-2011 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Common utilities used in testing"""
+
+import os
+import tempfile
+
+import fixtures
+from oslo.config import cfg
+import testtools
+
+from ironic.openstack.common.fixture import moxstubout
+
+
+class BaseTestCase(testtools.TestCase):
+
+    def setUp(self, conf=cfg.CONF):
+        super(BaseTestCase, self).setUp()
+        moxfixture = self.useFixture(moxstubout.MoxStubout())
+        self.mox = moxfixture.mox
+        self.stubs = moxfixture.stubs
+        self.conf = conf
+        self.addCleanup(self.conf.reset)
+        self.useFixture(fixtures.FakeLogger('openstack.common'))
+        self.useFixture(fixtures.Timeout(30, True))
+        self.config(fatal_exception_format_errors=True)
+        self.useFixture(fixtures.NestedTempfile())
+        self.tempdirs = []
+
+    def tearDown(self):
+        super(BaseTestCase, self).tearDown()
+        self.conf.reset()
+        self.stubs.UnsetAll()
+        self.stubs.SmartUnsetAll()
+
+    def create_tempfiles(self, files, ext='.conf'):
+        tempfiles = []
+        for (basename, contents) in files:
+            if not os.path.isabs(basename):
+                (fd, path) = tempfile.mkstemp(prefix=basename, suffix=ext)
+            else:
+                path = basename + ext
+                fd = os.open(path, os.O_CREAT | os.O_WRONLY)
+            tempfiles.append(path)
+            try:
+                os.write(fd, contents)
+            finally:
+                os.close(fd)
+        return tempfiles
+
+    def config(self, **kw):
+        """Override some configuration values.
+
+        The keyword arguments are the names of configuration options to
+        override and their values.
+
+        If a group argument is supplied, the overrides are applied to
+        the specified configuration option group.
+
+        All overrides are automatically cleared at the end of the current
+        test by the tearDown() method.
+        """
+        group = kw.pop('group', None)
+        for k, v in kw.iteritems():
+            self.conf.set_override(k, v, group)
