d9160d95c7159f934d552b0f233643cae83a5ca1
Dirk Mueller <dirk@dmllr.de>
2013-09-16 23:17:41 +0200

    Use localisation where user visible strings are used
    Change-Id: I224e3faaf7d6fcf0e3db807646028633a532a78b

--- ironic/cmd/ironic_deploy_helper.py
+++ ironic/cmd/ironic_deploy_helper.py
@@ -177,14 +177,14 @@ def work_on_disk(dev, root_mb, swap_mb, image_path):
     swap_part = "%s-part2" % dev
 
     if not is_block_device(dev):
-        LOG.warn("parent device '%s' not found", dev)
+        LOG.warn(_("parent device '%s' not found") % dev)
         return
     make_partitions(dev, root_mb, swap_mb)
     if not is_block_device(root_part):
-        LOG.warn("root device '%s' not found", root_part)
+        LOG.warn(_("root device '%s' not found") % root_part)
         return
     if not is_block_device(swap_part):
-        LOG.warn("swap device '%s' not found", swap_part)
+        LOG.warn(_("swap device '%s' not found") % swap_part)
         return
     dd(image_path, root_part)
     mkswap(swap_part)
@@ -276,7 +276,7 @@ class BareMetalDeploy(object):
             return 'Not Implemented'
 
     def post(self, environ, start_response):
-        LOG.info("post: environ=%s", environ)
+        LOG.info(_("post: environ=%s") % environ)
         inpt = environ['wsgi.input']
         length = int(environ.get('CONTENT_LENGTH', 0))
 
@@ -317,7 +317,8 @@ class BareMetalDeploy(object):
         if not self.worker.isAlive():
             self.worker = Worker()
             self.worker.start()
-        LOG.info("request is queued: node %s, params %s", node_id, params)
+        LOG.info(_("request is queued: node %(node_id)s, params %(params)s") %
+                {'node_id': node_id, 'params': params})
         QUEUE.put((node_id, params))
         # Requests go to Worker.run()
         start_response('200 OK', [('Content-type', 'text/plain')])
