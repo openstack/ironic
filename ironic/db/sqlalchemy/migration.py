a5b2820686417b48a1e5e477b4aaeed159d770b1
Devananda van der Veen <devananda.vdv@gmail.com>
2013-11-15 14:52:57 -0800

    Remove sqlalchemy-migrate 0.7.3 patching
    Now that sqlalchemy-migrate 0.8.x is available, remove the 0.7.3
    patching. This allows environments to pick up 0.8.1 automatically, while
    avoiding the version string error. Once an update to global-requirements
    is made, another patch will pin our sqlalchemy-migrate version >= 0.8.2.
    
    This is copied from change I5f80dfc07f8256716d025002fc1a1f47e58b2142
    
    Change-Id: I646a5a8f427095e20b09fbf2db796d2644def6fc
    Closes-bug: 1251700

--- ironic/db/sqlalchemy/migration.py
+++ ironic/db/sqlalchemy/migration.py
@@ -16,38 +16,13 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import distutils.version as dist_version
 import os
 
-import migrate
-from migrate.versioning import util as migrate_util
 import sqlalchemy
 
 from ironic.common import exception
 from ironic.db import migration
 from ironic.openstack.common.db.sqlalchemy import session as db_session
-
-
-@migrate_util.decorator
-def patched_with_engine(f, *a, **kw):
-    url = a[0]
-    engine = migrate_util.construct_engine(url, **kw)
-
-    try:
-        kw['engine'] = engine
-        return f(*a, **kw)
-    finally:
-        if isinstance(engine, migrate_util.Engine) and engine is not url:
-            migrate_util.log.debug('Disposing SQLAlchemy engine %s', engine)
-            engine.dispose()
-
-
-# TODO(jkoelker) When migrate 0.7.3 is released and nova depends
-#                on that version or higher, this can be removed
-MIN_PKG_VERSION = dist_version.StrictVersion('0.7.3')
-if (not hasattr(migrate, '__version__') or
-    dist_version.StrictVersion(migrate.__version__) < MIN_PKG_VERSION):
-    migrate_util.with_engine = patched_with_engine
 
 
 # NOTE(jkoelker) Delay importing migrate until we are patched
