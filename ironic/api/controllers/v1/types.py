e8dd8f3cfb6a2659e38d7167b3b9093ce815e306
Yuriy Zveryanskyy <yzveryanskyy@mirantis.com>
2014-02-13 13:36:01 +0200

    Use StringType from WSME
    After WSME 0.6 released we can use StringType class from wsme.types
    and remove it from Ironic code.
    
    Change-Id: I00378bb126a1d786330b7cad3ac671d992a3109d

--- ironic/api/controllers/v1/types.py
+++ ironic/api/controllers/v1/types.py
@@ -16,9 +16,6 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import re
-import six
-
 import wsme
 from wsme import types as wtypes
 
@@ -69,57 +66,11 @@ macaddress = MacAddressType()
 uuid = UuidType()
 
 
-# TODO(lucasagomes): WSME already has this StringType implementation on trunk,
-#                    so remove it on the next WSME release (> 0.5b6)
-class StringType(wtypes.UserType):
-    """A simple string type. Can validate a length and a pattern.
-
-    :param min_length: Possible minimum length
-    :param max_length: Possible maximum length
-    :param pattern: Possible string pattern
-
-    Example::
-
-    Name = StringType(min_length=1, pattern='^[a-zA-Z ]*$')
-
-    """
-    basetype = six.string_types
-    name = "string"
-
-    def __init__(self, min_length=None, max_length=None, pattern=None):
-        self.min_length = min_length
-        self.max_length = max_length
-        if isinstance(pattern, six.string_types):
-            self.pattern = re.compile(pattern)
-        else:
-            self.pattern = pattern
-
-    def validate(self, value):
-        if not isinstance(value, self.basetype):
-            error = 'Value should be string'
-            raise ValueError(error)
-
-        if self.min_length is not None and len(value) < self.min_length:
-            error = 'Value should have a minimum character requirement of %s' \
-                    % self.min_length
-            raise ValueError(error)
-
-        if self.max_length is not None and len(value) > self.max_length:
-            error = 'Value should have a maximum character requirement of %s' \
-                    % self.max_length
-            raise ValueError(error)
-
-        if self.pattern is not None and not self.pattern.match(value):
-            error = 'Value should match the pattern %s' % self.pattern.pattern
-            raise ValueError(error)
-
-        return value
-
-
 class JsonPatchType(wtypes.Base):
     """A complex type that represents a single json-patch operation."""
 
-    path = wtypes.wsattr(StringType(pattern='^(/[\w-]+)+$'), mandatory=True)
+    path = wtypes.wsattr(wtypes.StringType(pattern='^(/[\w-]+)+$'),
+                         mandatory=True)
     op = wtypes.wsattr(wtypes.Enum(str, 'add', 'replace', 'remove'),
                        mandatory=True)
     value = wtypes.text
