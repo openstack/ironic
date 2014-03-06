d32d6ab4f5f3cb8b84d69dc1be0424b82eb1a2a4
Lucas Alvares Gomes <lucasagomes@gmail.com>
2014-03-06 17:49:32 +0000

    Fix how nova ironic driver gets flavor information
    Nova changed the way it access the flavor information, they now use the
    Flavor object instead of getting it throught the VirtAPI.get_flavor()
    call.
    
    Also fix the import order in the driver.py
    
    Change-Id: I4308d9c994a13f63003ee9426229b51063926c00

--- ironic/nova/virt/ironic/driver.py
+++ ironic/nova/virt/ironic/driver.py
@@ -25,9 +25,11 @@ from ironicclient import client as ironic_client
 from ironicclient import exc as ironic_exception
 from oslo.config import cfg
 
+from ironic.nova.virt.ironic import ironic_states
 from nova.compute import power_state
 from nova import context as nova_context
 from nova import exception
+from nova.objects import flavor as flavor_obj
 from nova.openstack.common import excutils
 from nova.openstack.common.gettextutils import _
 from nova.openstack.common import importutils
@@ -36,7 +38,6 @@ from nova.openstack.common import log as logging
 from nova.openstack.common import loopingcall
 from nova.virt import driver as virt_driver
 from nova.virt import firewall
-from ironic.nova.virt.ironic import ironic_states
 
 LOG = logging.getLogger(__name__)
 
@@ -387,7 +388,8 @@ class IronicDriver(virt_driver.ComputeDriver):
 
         # Set image id, and other driver info so we can pass it down to Ironic
         # use the ironic_driver_fields file to import
-        flavor = self.virtapi.flavor_get(context, instance['instance_type_id'])
+        flavor = flavor_obj.Flavor.get_by_id(context,
+                                             instance['instance_type_id'])
         self._add_driver_fields(node, instance, image_meta, flavor)
 
         #validate we ready to do the deploy
