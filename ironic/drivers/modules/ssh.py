35605f99b96ad8a39152d301fad1827327abd66f
Robert Collins <rbtcollins@hp.com>
2014-03-14 08:27:21 +1300

    Permit passing SSH keys into the Ironic API
    SSH keys are equivalent to passwords for the ssh driver, and having to
    special case deployment metadata to push them out of band to a file is
    awkward. This patch adds the ability to pass the key in directly which
    is much easier (in some environments).
    
    Change-Id: I4421c80cb3dc0c8aee614c684d113a7ab6db6779

--- ironic/drivers/modules/ssh.py
+++ ironic/drivers/modules/ssh.py
@@ -158,6 +158,7 @@ def _parse_driver_info(node):
     except ValueError:
         raise exception.InvalidParameterValue(_(
             "SSHPowerDriver requires ssh_port to be integer value"))
+    key_contents = info.get('ssh_key_contents')
     key_filename = info.get('ssh_key_filename')
     virt_type = info.get('ssh_virt_type')
 
@@ -180,13 +181,17 @@ def _parse_driver_info(node):
     if not address or not username:
         raise exception.InvalidParameterValue(_(
             "SSHPowerDriver requires both address and username be set."))
+    # Only one credential may be set (avoids complexity around having
+    # precedence etc).
+    if len(filter(None, (password, key_filename, key_contents))) != 1:
+        raise exception.InvalidParameterValue(_(
+            "SSHPowerDriver requires one and only one of password, "
+            "key_contents and key_filename to be set."))
     if password:
         res['password'] = password
+    elif key_contents:
+        res['key_contents'] = key_contents
     else:
-        if not key_filename:
-            raise exception.InvalidParameterValue(_(
-                "SSHPowerDriver requires either password or "
-                "key_filename be set."))
         if not os.path.isfile(key_filename):
             raise exception.InvalidParameterValue(_(
                 "SSH key file %s not found.") % key_filename)
