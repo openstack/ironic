8e854d7eb8c5d98efed34c27cc55cac49d77d7f9
Devananda van der Veen <devananda.vdv@gmail.com>
2014-03-18 11:22:37 -0700

    Correct version.py and update current version string
    Correct the version string in setup.cfg for Icehouse.
    
    Make ironic/version.py use PBR instead of the stale code that was there.
    
    Make ironic/common/config.py properly set the version string so that
    all CLI commands output it when "--version" is passed.
    
    Remove openstack.common.version module which is no longer present in
    oslo-incubator.
    
    Closes-bug: #1294389
    
    Change-Id: I9a7bfe5fc1b79934cf2467d6f8a6c16f41205dbf

--- ironic/version.py
+++ ironic/version.py
@@ -13,33 +13,6 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-try:
-    from ironic.vcsversion import version_info
-except ImportError:
-    version_info = {'branch_nick': u'LOCALBRANCH',
-                    'revision_id': 'LOCALREVISION',
-                    'revno': 0}
+import pbr.version
 
-IRONIC_VERSION = ['2013', '1']
-YEAR, COUNT = IRONIC_VERSION
-
-FINAL = False   # This becomes true at Release Candidate time
-
-
-def canonical_version_string():
-    return '.'.join([YEAR, COUNT])
-
-
-def version_string():
-    if FINAL:
-        return canonical_version_string()
-    else:
-        return '%s-dev' % (canonical_version_string(),)
-
-
-def vcs_version_string():
-    return "%s:%s" % (version_info['branch_nick'], version_info['revision_id'])
-
-
-def version_string_with_vcs():
-    return "%s-%s" % (canonical_version_string(), vcs_version_string())
+version_info = pbr.version.VersionInfo('ironic')
