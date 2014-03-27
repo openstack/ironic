4bdd6afb5a7abfd5ccf2e0095cacadbf9fcc62dd
Lucas Alvares Gomes <lucasagomes@gmail.com>
2014-03-27 14:27:56 +0000

    Add DiskPartitioner
    DiskPartitioner is a convenient wrapper around the parted tool, this
    wrapper will make things easier for fixing other problems in Ironic like
    removing the swap partition if not specified, having a more consistent
    partition layout or future work like creating GPT (GUID Partition Table)
    partitions (needed for creating partitions with size bigger than 2TB),
    efi partitions, etc..
    
    Change-Id: I44be217d1e55778ed94f27603ce0537ff740febf
    Partial-Bug: #1297925
    Partial-Bug: #1298362

---
+++ ironic/tests/test_disk_partitioner.py
@@ -0,0 +1,70 @@
+# Copyright 2014 Red Hat, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+from testtools.matchers import HasLength
+
+from ironic.common import disk_partitioner
+from ironic.common import utils
+from ironic.tests import base
+
+
+class DiskPartitionerTestCase(base.TestCase):
+
+    def setUp(self):
+        super(DiskPartitionerTestCase, self).setUp()
+        self.mock_exc = mock.patch.object(utils, 'execute')
+        self.mock_exc.start()
+        self.addCleanup(self.mock_exc.stop)
+
+    def test_add_partition(self):
+        dp = disk_partitioner.DiskPartitioner('/dev/fake')
+        dp.add_partition(1024)
+        dp.add_partition(512, fs_type='linux-swap')
+        dp.add_partition(2048, bootable=True)
+        expected = [(1, {'bootable': False,
+                         'fs_type': '',
+                         'type': 'primary',
+                         'size': 1024}),
+                    (2, {'bootable': False,
+                         'fs_type': 'linux-swap',
+                         'type': 'primary',
+                         'size': 512}),
+                    (3, {'bootable': True,
+                         'fs_type': '',
+                         'type': 'primary',
+                         'size': 2048})]
+        partitions = [(n, p) for n, p in dp.get_partitions()]
+        self.assertThat(partitions, HasLength(3))
+        self.assertEqual(expected, partitions)
+
+    @mock.patch.object(disk_partitioner.DiskPartitioner, '_exec')
+    def test_commit(self, mock_exec):
+        dp = disk_partitioner.DiskPartitioner('/dev/fake')
+        fake_parts = [(1, {'bootable': False,
+                           'fs_type': 'fake-fs-type',
+                           'type': 'fake-type',
+                           'size': 1}),
+                      (2, {'bootable': True,
+                           'fs_type': 'fake-fs-type',
+                           'type': 'fake-type',
+                           'size': 1})]
+        with mock.patch.object(dp, 'get_partitions') as mock_gp:
+            mock_gp.return_value = fake_parts
+            dp.commit()
+        mock_exec.assert_called_once_with('mklabel', 'msdos',
+                               'mkpart', 'fake-type', 'fake-fs-type', '1', '2',
+                               'mkpart', 'fake-type', 'fake-fs-type', '2', '3',
+                               'set', '2', 'boot', 'on')
