b79aeb58a9cef67b471863d4940f33bf4cd044d5
Tushar Kalra <tushar@cloudscaling.com>
2014-04-17 13:45:53 -0700

    Remove hardcoded node id value
    Change-Id: I9e4e74ce3a41e1cd3dd4e0d9ff1d1bde0656527f
    Closes-Bug: 1306214

--- ironic/tests/drivers/test_utils.py
+++ ironic/tests/drivers/test_utils.py
@@ -79,14 +79,14 @@ class UtilsTestCase(base.TestCase):
                     id=6,
                     address='aa:bb:cc',
                     uuid='bb43dc0b-03f2-4d2e-ae87-c02d7f33cc53',
-                    node_id='123')))
+                    node_id=self.node.id)))
         ports.append(
             self.dbapi.create_port(
                 db_utils.get_test_port(
                     id=7,
                     address='dd:ee:ff',
                     uuid='4fc26c0b-03f2-4d2e-ae87-c02d7f33c234',
-                    node_id='123')))
+                    node_id=self.node.id)))
         with task_manager.acquire(self.context, [self.node.uuid]) as task:
             node_macs = driver_utils.get_node_mac_addresses(task, self.node)
         self.assertEqual(sorted([p.address for p in ports]), sorted(node_macs))
