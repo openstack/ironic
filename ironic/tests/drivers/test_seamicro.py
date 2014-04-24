990d6545fb7b3488769f7e2d15382a6f895a67b9
Devananda van der Veen <devananda.vdv@gmail.com>
2014-04-24 11:10:32 -0700

    Mock seamicroclient lib in unit tests if not present
    Mock the python-seamicroclient library from within ironic/tests/drivers
    so that test_seamicro.py can import ironic/drivers/modules/seamicro.py
    and run the unit tests on it, even when the external library is not
    present.
    
    Change-Id: I4f2cd75b2c64a5a280a5d45d5cea058fd432815b
    Closes-bug: #1312321

--- ironic/tests/drivers/test_seamicro.py
+++ ironic/tests/drivers/test_seamicro.py
@@ -15,6 +15,7 @@
 import uuid
 
 import mock
+from seamicroclient import exceptions as seamicro_client_exception
 
 from ironic.common import driver_factory
 from ironic.common import exception
@@ -23,7 +24,6 @@ from ironic.conductor import task_manager
 from ironic.db import api as dbapi
 from ironic.drivers.modules import seamicro
 from ironic.openstack.common import context
-from ironic.openstack.common import importutils
 from ironic.tests import base
 from ironic.tests.conductor import utils as mgr_utils
 from ironic.tests.db import base as db_base
@@ -31,10 +31,6 @@ from ironic.tests.db import utils as db_utils
 from ironic.tests.objects import utils as obj_utils
 
 INFO_DICT = db_utils.get_test_seamicro_info()
-
-seamicroclient = importutils.try_import("seamicroclient")
-if seamicroclient:
-    from seamicroclient import exceptions as seamicro_client_exception
 
 
 class Fake_Server():
@@ -264,8 +260,6 @@ class SeaMicroPowerDriverTestCase(db_base.DbTestCase):
 
     def setUp(self):
         super(SeaMicroPowerDriverTestCase, self).setUp()
-        if not seamicroclient:
-            self.skipTest("Seamicroclient library not found")
         mgr_utils.mock_the_extension_manager(driver='fake_seamicro')
         self.driver = driver_factory.get_driver('fake_seamicro')
         self.node = obj_utils.create_test_node(self.context,
