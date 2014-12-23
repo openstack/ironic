46d24b51de16a8d743cbf505a6aebd0072aecaa6
ChangBo Guo(gcb) <eric.guo@easystack.cn>
2014-12-23 16:25:10 +0800

    Use get_my_ipv4 from oslo.utils
    Remove _get_my_ip from ironic and replace it's usages
    by usages of get_my_ipv4 from netutils, because this methods
    implements the same functionality.
    
    Change-Id: I85b75f26051170c0d306ef38b517cc1657e47a99

--- ironic/netconf.py
+++ ironic/netconf.py
@@ -15,34 +15,14 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import socket
-
 from oslo.config import cfg
+from oslo.utils import netutils
 
 CONF = cfg.CONF
 
-
-def _get_my_ip():
-    """Returns the actual ip of the local machine.
-
-    This code figures out what source address would be used if some traffic
-    were to be sent out to some well known address on the Internet. In this
-    case, a Google DNS server is used, but the specific address does not
-    matter much.  No traffic is actually sent.
-    """
-    try:
-        csock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        csock.connect(('8.8.8.8', 80))
-        (addr, port) = csock.getsockname()
-        csock.close()
-        return addr
-    except socket.error:
-        return "127.0.0.1"
-
-
 netconf_opts = [
     cfg.StrOpt('my_ip',
-               default=_get_my_ip(),
+               default=netutils.get_my_ipv4(),
                help='IP address of this host.'),
 ]
 
