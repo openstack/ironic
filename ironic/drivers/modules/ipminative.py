0b10b18bf1ff01ef8dfdd1e65ea0e9ac60f87315
John Trowbridge <trown@redhat.com>
2015-02-18 11:10:35 -0500

    Add documentation for the IPMI retry timeout option
    It is difficult to have a universal default for the retry_timeout
    option in the [impi] configuration section. This patch adds
    documentation of the tradeoffs involved, while leaving the
    conservative default in place.
    
    Closes-Bug: 1383432
    Change-Id: Ic7973cf7fee60cc817e5c0f5f9bfc84b3bca91c7

--- ironic/drivers/modules/ipminative.py
+++ ironic/drivers/modules/ipminative.py
@@ -46,7 +46,12 @@ if pyghmi:
 opts = [
     cfg.IntOpt('retry_timeout',
                default=60,
-               help='Maximum time in seconds to retry IPMI operations.'),
+               help='Maximum time in seconds to retry IPMI operations. There '
+                    'is a tradeoff when setting this value. Setting this too '
+                    'low may cause older BMCs to crash and require a hard '
+                    'reset. However, setting too high can cause the sync '
+                    'power state periodic task to hang when there are slow '
+                    'or unresponsive BMCs.'),
     cfg.IntOpt('min_command_interval',
                default=5,
                help='Minimum time, in seconds, between IPMI operations '
