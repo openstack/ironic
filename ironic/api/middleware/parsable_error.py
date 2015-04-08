eed8257e51fa071b9299f1ab33c14cc687f40fe4
Yuriy Zveryanskyy <yzveryanskyy@mirantis.com>
2015-04-08 12:28:21 +0300

    Fix type of value in error middleware response header
    Apache mod_wsgi is strict about WSGI specification violations,
    all response headers must be strings. This patch fixes value
    of 'Content-Length' header.
    
    Closes-Bug: #1441014
    Change-Id: Iab82cac883efb19886527c35a2cb0055cbe0a6ce

--- ironic/api/middleware/parsable_error.py
+++ ironic/api/middleware/parsable_error.py
@@ -85,7 +85,7 @@ class ParsableErrorMiddleware(object):
             else:
                 body = [json.dumps({'error_message': '\n'.join(app_iter)})]
                 state['headers'].append(('Content-Type', 'application/json'))
-            state['headers'].append(('Content-Length', len(body[0])))
+            state['headers'].append(('Content-Length', str(len(body[0]))))
         else:
             body = app_iter
         return body
