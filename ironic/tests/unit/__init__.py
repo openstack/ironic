5b4684b3a5b2c37c23fb83bc14ceda6cf7c01412
Vladyslav Drok <vdrok@mirantis.com>
2015-10-11 02:10:51 +0300

    Stop adding translation function to builtins
    In unittests __init__ translation function is still being added to
    builtins, this is not required anymore as it is not being installed.
    
    Change-Id: I19da395b72622a6db348f5a6dd569c7747eaa40d

--- ironic/tests/unit/__init__.py
+++ ironic/tests/unit/__init__.py
@@ -27,8 +27,3 @@
 import eventlet
 
 eventlet.monkey_patch(os=False)
-
-# See http://code.google.com/p/python-nose/issues/detail?id=373
-# The code below enables nosetests to work with i18n _() blocks
-import six.moves.builtins as __builtin__
-setattr(__builtin__, '_', lambda x: x)
