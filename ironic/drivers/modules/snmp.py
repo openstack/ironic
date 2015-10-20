9365a22c92fd00816dc4fd2c2de3d8f463fe24e5
Naohiro Tamura <naohirot@jp.fujitsu.com>
2015-10-20 13:59:35 +0900

    Fix snmp property descriptions
    This patch fixes snmp property descriptions having applied the same
    gerrit review comment [1].
    
    [1] https://review.openstack.org/#/c/196480/16/ironic/drivers/modules/irmc/common.py
    
    Change-Id: I6355088364a84ae8ca7396dbdb033f85b9f957ca

--- ironic/drivers/modules/snmp.py
+++ ironic/drivers/modules/snmp.py
@@ -84,13 +84,13 @@ REQUIRED_PROPERTIES = {
 }
 OPTIONAL_PROPERTIES = {
     'snmp_version':
-        _("SNMP protocol version: %(v1)s, %(v2c)s, %(v3)s  "
+        _("SNMP protocol version: %(v1)s, %(v2c)s or %(v3)s  "
           "(optional, default %(v1)s)")
         % {"v1": SNMP_V1, "v2c": SNMP_V2C, "v3": SNMP_V3},
     'snmp_port':
         _("SNMP port, default %(port)d") % {"port": SNMP_PORT},
     'snmp_community':
-        _("SNMP community.  Required for versions %(v1)s, %(v2c)s")
+        _("SNMP community.  Required for versions %(v1)s and %(v2c)s")
         % {"v1": SNMP_V1, "v2c": SNMP_V2C},
     'snmp_security':
         _("SNMP security name.  Required for version %(v3)s")
