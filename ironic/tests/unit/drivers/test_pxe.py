dd7d2f5ab2e1d71b5e1f0d8e65b83a530301fd70
Shuquan Huang <huang.shuquan@99cloud.net>
2015-11-06 13:10:16 +0000

    Add missing unit tests for some PXE drivers
    Some of the pxe_* drivers don't have unit tests, while some others have.
    Unit tests need to be available for other drivers as well.
    
    Change-Id: I6be2d65c00e575bb0df7309f0e891fdf37cb731f
    Closes-bug: #1512966

--- ironic/tests/unit/drivers/test_pxe.py
+++ ironic/tests/unit/drivers/test_pxe.py
@@ -21,16 +21,31 @@ import testtools
 
 from ironic.common import exception
 from ironic.drivers.modules import agent
+from ironic.drivers.modules.amt import management as amt_management
+from ironic.drivers.modules.amt import power as amt_power
+from ironic.drivers.modules.amt import vendor as amt_vendor
+from ironic.drivers.modules.cimc import management as cimc_management
+from ironic.drivers.modules.cimc import power as cimc_power
+from ironic.drivers.modules import iboot
 from ironic.drivers.modules.ilo import deploy as ilo_deploy
 from ironic.drivers.modules.ilo import inspect as ilo_inspect
 from ironic.drivers.modules.ilo import management as ilo_management
 from ironic.drivers.modules.ilo import power as ilo_power
 from ironic.drivers.modules import ipminative
 from ironic.drivers.modules import ipmitool
+from ironic.drivers.modules.irmc import management as irmc_management
+from ironic.drivers.modules.irmc import power as irmc_power
 from ironic.drivers.modules import iscsi_deploy
+from ironic.drivers.modules.msftocs import management as msftocs_management
+from ironic.drivers.modules.msftocs import power as msftocs_power
 from ironic.drivers.modules import pxe as pxe_module
+from ironic.drivers.modules import seamicro
+from ironic.drivers.modules import snmp
 from ironic.drivers.modules import ssh
+from ironic.drivers.modules.ucs import management as ucs_management
+from ironic.drivers.modules.ucs import power as ucs_power
 from ironic.drivers.modules import virtualbox
+from ironic.drivers.modules import wol
 from ironic.drivers import pxe
 from ironic.drivers import utils
 
@@ -90,6 +105,50 @@ class PXEDriversTestCase(testtools.TestCase):
 
     @mock.patch.object(pxe.importutils, 'try_import', spec_set=True,
                        autospec=True)
+    def test_pxe_seamicro_driver(self, try_import_mock):
+        try_import_mock.return_value = True
+
+        driver = pxe.PXEAndSeaMicroDriver()
+
+        self.assertIsInstance(driver.power, seamicro.Power)
+        self.assertIsInstance(driver.boot, pxe_module.PXEBoot)
+        self.assertIsInstance(driver.deploy, iscsi_deploy.ISCSIDeploy)
+        self.assertIsInstance(driver.management, seamicro.Management)
+        self.assertIsInstance(driver.seamicro_vendor, seamicro.VendorPassthru)
+        self.assertIsInstance(driver.pxe_vendor, iscsi_deploy.VendorPassthru)
+        self.assertIsInstance(driver.vendor, utils.MixinVendorInterface)
+        self.assertIsInstance(driver.console, seamicro.ShellinaboxConsole)
+
+    @mock.patch.object(pxe.importutils, 'try_import', spec_set=True,
+                       autospec=True)
+    def test_pxe_seamicro_driver_import_error(self, try_import_mock):
+        try_import_mock.return_value = False
+
+        self.assertRaises(exception.DriverLoadError,
+                          pxe.PXEAndSeaMicroDriver)
+
+    @mock.patch.object(pxe.importutils, 'try_import', spec_set=True,
+                       autospec=True)
+    def test_pxe_iboot_driver(self, try_import_mock):
+        try_import_mock.return_value = True
+
+        driver = pxe.PXEAndIBootDriver()
+
+        self.assertIsInstance(driver.power, iboot.IBootPower)
+        self.assertIsInstance(driver.boot, pxe_module.PXEBoot)
+        self.assertIsInstance(driver.deploy, iscsi_deploy.ISCSIDeploy)
+        self.assertIsInstance(driver.vendor, iscsi_deploy.VendorPassthru)
+
+    @mock.patch.object(pxe.importutils, 'try_import', spec_set=True,
+                       autospec=True)
+    def test_pxe_iboot_driver_import_error(self, try_import_mock):
+        try_import_mock.return_value = False
+
+        self.assertRaises(exception.DriverLoadError,
+                          pxe.PXEAndIBootDriver)
+
+    @mock.patch.object(pxe.importutils, 'try_import', spec_set=True,
+                       autospec=True)
     def test_pxe_ilo_driver(self, try_import_mock):
         try_import_mock.return_value = True
 
@@ -116,6 +175,50 @@ class PXEDriversTestCase(testtools.TestCase):
 
     @mock.patch.object(pxe.importutils, 'try_import', spec_set=True,
                        autospec=True)
+    def test_pxe_snmp_driver(self, try_import_mock):
+        try_import_mock.return_value = True
+
+        driver = pxe.PXEAndSNMPDriver()
+
+        self.assertIsInstance(driver.power, snmp.SNMPPower)
+        self.assertIsInstance(driver.boot, pxe_module.PXEBoot)
+        self.assertIsInstance(driver.deploy, iscsi_deploy.ISCSIDeploy)
+        self.assertIsInstance(driver.vendor, iscsi_deploy.VendorPassthru)
+        self.assertIsNone(driver.management)
+
+    @mock.patch.object(pxe.importutils, 'try_import', spec_set=True,
+                       autospec=True)
+    def test_pxe_snmp_driver_import_error(self, try_import_mock):
+        try_import_mock.return_value = False
+
+        self.assertRaises(exception.DriverLoadError,
+                          pxe.PXEAndSNMPDriver)
+
+    @mock.patch.object(pxe.importutils, 'try_import', spec_set=True,
+                       autospec=True)
+    def test_pxe_irmc_driver(self, try_import_mock):
+        try_import_mock.return_value = True
+
+        driver = pxe.PXEAndIRMCDriver()
+
+        self.assertIsInstance(driver.power, irmc_power.IRMCPower)
+        self.assertIsInstance(driver.console, ipmitool.IPMIShellinaboxConsole)
+        self.assertIsInstance(driver.boot, pxe_module.PXEBoot)
+        self.assertIsInstance(driver.deploy, iscsi_deploy.ISCSIDeploy)
+        self.assertIsInstance(driver.management,
+                              irmc_management.IRMCManagement)
+        self.assertIsInstance(driver.vendor, iscsi_deploy.VendorPassthru)
+
+    @mock.patch.object(pxe.importutils, 'try_import', spec_set=True,
+                       autospec=True)
+    def test_pxe_irmc_driver_import_error(self, try_import_mock):
+        try_import_mock.return_value = False
+
+        self.assertRaises(exception.DriverLoadError,
+                          pxe.PXEAndIRMCDriver)
+
+    @mock.patch.object(pxe.importutils, 'try_import', spec_set=True,
+                       autospec=True)
     def test_pxe_vbox_driver(self, try_import_mock):
         try_import_mock.return_value = True
 
@@ -136,3 +239,95 @@ class PXEDriversTestCase(testtools.TestCase):
 
         self.assertRaises(exception.DriverLoadError,
                           pxe.PXEAndVirtualBoxDriver)
+
+    @mock.patch.object(pxe.importutils, 'try_import', spec_set=True,
+                       autospec=True)
+    def test_pxe_amt_driver(self, try_import_mock):
+        try_import_mock.return_value = True
+
+        driver = pxe.PXEAndAMTDriver()
+
+        self.assertIsInstance(driver.power, amt_power.AMTPower)
+        self.assertIsInstance(driver.boot, pxe_module.PXEBoot)
+        self.assertIsInstance(driver.deploy, iscsi_deploy.ISCSIDeploy)
+        self.assertIsInstance(driver.management,
+                              amt_management.AMTManagement)
+        self.assertIsInstance(driver.vendor, amt_vendor.AMTPXEVendorPassthru)
+
+    @mock.patch.object(pxe.importutils, 'try_import', spec_set=True,
+                       autospec=True)
+    def test_pxe_amt_driver_import_error(self, try_import_mock):
+        try_import_mock.return_value = False
+
+        self.assertRaises(exception.DriverLoadError,
+                          pxe.PXEAndAMTDriver)
+
+    @mock.patch.object(pxe.importutils, 'try_import', spec_set=True,
+                       autospec=True)
+    def test_pxe_msftocs_driver(self, try_import_mock):
+        try_import_mock.return_value = True
+
+        driver = pxe.PXEAndMSFTOCSDriver()
+
+        self.assertIsInstance(driver.power, msftocs_power.MSFTOCSPower)
+        self.assertIsInstance(driver.boot, pxe_module.PXEBoot)
+        self.assertIsInstance(driver.deploy, iscsi_deploy.ISCSIDeploy)
+        self.assertIsInstance(driver.management,
+                              msftocs_management.MSFTOCSManagement)
+        self.assertIsInstance(driver.vendor, iscsi_deploy.VendorPassthru)
+
+    @mock.patch.object(pxe.importutils, 'try_import', spec_set=True,
+                       autospec=True)
+    def test_pxe_ucs_driver(self, try_import_mock):
+        try_import_mock.return_value = True
+
+        driver = pxe.PXEAndUcsDriver()
+
+        self.assertIsInstance(driver.power, ucs_power.Power)
+        self.assertIsInstance(driver.boot, pxe_module.PXEBoot)
+        self.assertIsInstance(driver.deploy, iscsi_deploy.ISCSIDeploy)
+        self.assertIsInstance(driver.management,
+                              ucs_management.UcsManagement)
+        self.assertIsInstance(driver.vendor, iscsi_deploy.VendorPassthru)
+
+    @mock.patch.object(pxe.importutils, 'try_import', spec_set=True,
+                       autospec=True)
+    def test_pxe_ucs_driver_import_error(self, try_import_mock):
+        try_import_mock.return_value = False
+
+        self.assertRaises(exception.DriverLoadError,
+                          pxe.PXEAndUcsDriver)
+
+    @mock.patch.object(pxe.importutils, 'try_import', spec_set=True,
+                       autospec=True)
+    def test_pxe_cimc_driver(self, try_import_mock):
+        try_import_mock.return_value = True
+
+        driver = pxe.PXEAndCIMCDriver()
+
+        self.assertIsInstance(driver.power, cimc_power.Power)
+        self.assertIsInstance(driver.boot, pxe_module.PXEBoot)
+        self.assertIsInstance(driver.deploy, iscsi_deploy.ISCSIDeploy)
+        self.assertIsInstance(driver.management,
+                              cimc_management.CIMCManagement)
+        self.assertIsInstance(driver.vendor, iscsi_deploy.VendorPassthru)
+
+    @mock.patch.object(pxe.importutils, 'try_import', spec_set=True,
+                       autospec=True)
+    def test_pxe_cimc_driver_import_error(self, try_import_mock):
+        try_import_mock.return_value = False
+
+        self.assertRaises(exception.DriverLoadError,
+                          pxe.PXEAndCIMCDriver)
+
+    @mock.patch.object(pxe.importutils, 'try_import', spec_set=True,
+                       autospec=True)
+    def test_pxe_wakeonlan_driver(self, try_import_mock):
+        try_import_mock.return_value = True
+
+        driver = pxe.PXEAndWakeOnLanDriver()
+
+        self.assertIsInstance(driver.power, wol.WakeOnLanPower)
+        self.assertIsInstance(driver.boot, pxe_module.PXEBoot)
+        self.assertIsInstance(driver.deploy, iscsi_deploy.ISCSIDeploy)
+        self.assertIsInstance(driver.vendor, iscsi_deploy.VendorPassthru)
