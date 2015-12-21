d7c41ebf9bc07f253fe08f77fde62a5a7ffef776
Lucas Alvares Gomes <lucasagomes@gmail.com>
2015-12-21 16:22:00 +0800

    Fix "No closing quotation" error when building with tox
    Apparently # is treated as a comment inside the string and as
    consequence the rest of the command gets stripped. This patch drops
    those comments from the bashate command.
    
    This patch also moves the bashate command to its own script under
    tools/.
    
    Change-Id: Ic44bb64419ca1465f69c91fd579e8c84d69f84b8
    Closes-Bug: #1527562

---
+++ tools/run_bashate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+find  "$@"  -not \( -type d -name .?\* -prune \)    \
+            -type f                                 \
+            -not -name \*.swp                       \
+            -not -name \*~                          \
+            -not -name \*.xml                       \
+            -not -name \*.template                  \
+            -not -wholename \*scripts/configure-vm  \
+            \(                                      \
+                -name \*.sh -or                     \
+                -wholename \*/lib/\* -or            \
+                -wholename \*/tools/\*              \
+            \)                                      \
+            -print0 | xargs -0 bashate -v -iE006 -eE005,E042
