cdd8d938f858ab9e3000a8e5840b45f90ce55a1f
Ruby Loo <ruby.loo@intel.com>
2016-05-30 10:56:04 -0400

    Remove support for 'hexraw' iPXE type
    This removes support for "hexraw" type in the iPXE script
    (boot.ipxe) since "hexraw" is not supported in older
    versions of iPXE. "hexhyp" replaced "hexraw" and has been
    used since kilo.
    
    Also addresses a nit in the original patch
    (https://review.openstack.org/#/c/171759/). For
    _get_pxe_mac_path(),  changed the assigned value of
    'delimiter' argument from None to '-' (because it
    was coded to be '-' as the default).
    
    Change-Id: Ide05ff7c4689cd696e2fe76be98ff99c0be24236

--- ironic/tests/unit/common/test_pxe_utils.py
+++ ironic/tests/unit/common/test_pxe_utils.py
@@ -238,17 +238,11 @@ class TestPXEUtils(db_base.DbTestCase):
             mock.call(u'../1be26c0b-03f2-4d2e-ae87-c02d7f33c123/config',
                       '/httpboot/pxelinux.cfg/00-11-22-33-44-55-66'),
             mock.call(u'../1be26c0b-03f2-4d2e-ae87-c02d7f33c123/config',
-                      '/httpboot/pxelinux.cfg/00112233445566'),
-            mock.call(u'../1be26c0b-03f2-4d2e-ae87-c02d7f33c123/config',
                       '/httpboot/pxelinux.cfg/00-11-22-33-44-55-67'),
-            mock.call(u'../1be26c0b-03f2-4d2e-ae87-c02d7f33c123/config',
-                      '/httpboot/pxelinux.cfg/00112233445567'),
         ]
         unlink_calls = [
             mock.call('/httpboot/pxelinux.cfg/00-11-22-33-44-55-66'),
-            mock.call('/httpboot/pxelinux.cfg/00112233445566'),
             mock.call('/httpboot/pxelinux.cfg/00-11-22-33-44-55-67'),
-            mock.call('/httpboot/pxelinux.cfg/00112233445567'),
         ]
         with task_manager.acquire(self.context, self.node.uuid) as task:
             pxe_utils._link_mac_pxe_configs(task)
@@ -614,10 +608,7 @@ class TestPXEUtils(db_base.DbTestCase):
             task.node.properties = properties
             pxe_utils.clean_up_pxe_config(task)
 
-            unlink_calls = [
-                mock.call('/httpboot/pxelinux.cfg/aa-aa-aa-aa-aa-aa'),
-                mock.call('/httpboot/pxelinux.cfg/aaaaaaaaaaaa')
-            ]
-            unlink_mock.assert_has_calls(unlink_calls)
+            unlink_mock.assert_called_once_with(
+                '/httpboot/pxelinux.cfg/aa-aa-aa-aa-aa-aa')
             rmtree_mock.assert_called_once_with(
                 os.path.join(CONF.deploy.http_root, self.node.uuid))
