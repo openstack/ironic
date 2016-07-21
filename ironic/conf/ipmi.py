4136f65d5d2c6230c834674301a5ef7d595fe3fe
Vasyl Saienko <vsaienko@mirantis.com>
2016-07-21 10:40:26 +0300

    Remove duplicate copyright.
    This patch removes duplicate copyright string from
    ironic/conf/ipmi.py
    
    Change-Id: I966d6a498af3647d54e944015824f63594d41219

--- ironic/conf/ipmi.py
+++ ironic/conf/ipmi.py
@@ -2,7 +2,6 @@
 #
 # Copyright 2013 International Business Machines Corporation
 # All Rights Reserved.
-# Copyright 2016 Intel Corporation
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
