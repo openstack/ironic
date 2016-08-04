fd9614dbb2d4b97826e8c3ae022a105bd7073b08
Vladyslav Drok <vdrok@mirantis.com>
2016-08-04 18:19:14 +0300

    Fix __all__ module attributes
    They should be iterables containing strings.
    
    Closes-bug: #1609853
    Change-Id: I1d0e044851323a9899b347b893f95a436e2a45aa

--- ironic/api/middleware/__init__.py
+++ ironic/api/middleware/__init__.py
@@ -19,5 +19,5 @@ from ironic.api.middleware import parsable_error
 ParsableErrorMiddleware = parsable_error.ParsableErrorMiddleware
 AuthTokenMiddleware = auth_token.AuthTokenMiddleware
 
-__all__ = (ParsableErrorMiddleware,
-           AuthTokenMiddleware)
+__all__ = ('ParsableErrorMiddleware',
+           'AuthTokenMiddleware')
