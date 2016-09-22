25b5edf00518120ea4dcd0767f5e3b83ee8c377a
Vladyslav Drok <vdrok@mirantis.com>
2016-09-22 18:15:03 +0300

    Increase timeout for migration-related tests
    Closes-Bug: #1626612
    Change-Id: I5836733d916cd04990efa7cf402cca4cfd8b7e24
    (cherry picked from commit 8813fc54cd689ec1ea2384e3a9acdb0f4d7d8e8b)

--- ironic/tests/unit/db/sqlalchemy/test_migrations.py
+++ ironic/tests/unit/db/sqlalchemy/test_migrations.py
@@ -36,6 +36,7 @@ For postgres on Ubuntu this can be done with the following commands:
 
 import collections
 import contextlib
+import fixtures
 
 from alembic import script
 import mock
@@ -55,6 +56,10 @@ from ironic.db.sqlalchemy import models
 from ironic.tests import base
 
 LOG = logging.getLogger(__name__)
+
+# NOTE(vdrok): This was introduced after migration tests started taking more
+# time in gate
+MIGRATIONS_TIMEOUT = 300
 
 
 def _get_connect_string(backend, user, passwd, database):
@@ -212,6 +217,8 @@ class MigrationCheckersMixin(object):
         super(MigrationCheckersMixin, self).setUp()
         self.config = migration._alembic_config()
         self.migration_api = migration
+        self.useFixture(fixtures.Timeout(MIGRATIONS_TIMEOUT,
+                                         gentle=True))
 
     def test_walk_versions(self):
         self._walk_versions(self.engine, self.config)
@@ -568,6 +575,11 @@ class TestMigrationsPostgreSQL(MigrationCheckersMixin,
 
 
 class ModelsMigrationSyncMixin(object):
+
+    def setUp(self):
+        super(ModelsMigrationSyncMixin, self).setUp()
+        self.useFixture(fixtures.Timeout(MIGRATIONS_TIMEOUT,
+                                         gentle=True))
 
     def get_metadata(self):
         return models.Base.metadata
