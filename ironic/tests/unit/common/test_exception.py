e8d93a420b76423443d0480ed391f10b158e11ad
Vladyslav Drok <vdrok@mirantis.com>
2016-11-21 19:35:23 +0200

    Make all IronicExceptions RPC-serializable
    Currently, if a random object is passed to the IronicException
    constructor, and then such exception needs to be sent over RPC,
    its serialization fails. In case of this message was supposed
    to be sent from conductor to API as response to an RPC call,
    API times out waiting for response and throws HTTP 500 error.
    This change makes sure that everything passed to the exception's
    constructor in kwargs is serializable, otherwise it's dropped
    from kwargs.
    
    Co-Authored-By: Richard Pioso <richard.pioso@dell.com>
    Closes-Bug: #1639982
    Change-Id: Icc153c6eefec7bd1451bc2110f742ae0241d49a1
    (cherry picked from commit bbbe0012d81e35ffa65eea9ccb1f8e7b96ae81c3)

--- ironic/tests/unit/common/test_exception.py
+++ ironic/tests/unit/common/test_exception.py
@@ -12,17 +12,65 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import re
+
+import mock
 import six
 
 from ironic.common import exception
 from ironic.tests import base
 
 
+class Unserializable(object):
+
+    def __str__(self):
+        raise NotImplementedError('nostr')
+
+
+class TestException(exception.IronicException):
+    _msg_fmt = 'Some exception: %(spam)s, %(ham)s'
+
+
 class TestIronicException(base.TestCase):
-    def test____init__(self):
+    def test___init__(self):
         expected = b'\xc3\xa9\xe0\xaf\xb2\xe0\xbe\x84'
         if six.PY3:
             expected = expected.decode('utf-8')
         message = six.unichr(233) + six.unichr(0x0bf2) + six.unichr(3972)
         exc = exception.IronicException(message)
         self.assertEqual(expected, exc.__str__())
+
+    @mock.patch.object(exception.LOG, 'error', autospec=True)
+    def test___init___invalid_kwarg(self, log_mock):
+        self.config(fatal_exception_format_errors=False)
+        e = TestException(spam=Unserializable(), ham='eggs')
+        message = log_mock.call_args[0][0] % log_mock.call_args[0][1]
+        self.assertIsNotNone(
+            re.search('spam: .*JSON.* ValueError: Circular reference detected;'
+                      '.*string.* NotImplementedError: nostr', message)
+        )
+        self.assertEqual({'ham': '"eggs"', 'code': 500}, e.kwargs)
+
+    @mock.patch.object(exception.LOG, 'error', autospec=True)
+    def test___init___invalid_kwarg_reraise(self, log_mock):
+        self.config(fatal_exception_format_errors=True)
+        self.assertRaises(KeyError, TestException, spam=Unserializable(),
+                          ham='eggs')
+        message = log_mock.call_args[0][0] % log_mock.call_args[0][1]
+        self.assertIsNotNone(
+            re.search('spam: .*JSON.* ValueError: Circular reference detected;'
+                      '.*string.* NotImplementedError: nostr', message)
+        )
+
+    def test___init___json_serializable(self):
+        exc = TestException(spam=[1, 2, 3], ham='eggs')
+        self.assertIn('[1, 2, 3]', six.text_type(exc))
+        self.assertEqual('[1, 2, 3]', exc.kwargs['spam'])
+
+    def test___init___string_serializable(self):
+        exc = TestException(
+            spam=type('ni', (object,), dict(a=1, b=2))(), ham='eggs'
+        )
+        check_str = 'ni object at'
+        self.assertIn(check_str, six.text_type(exc))
+        self.assertIn(check_str, exc.kwargs['spam'])
