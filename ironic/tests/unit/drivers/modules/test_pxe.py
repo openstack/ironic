2bc984aabacce75f01fa0879b7783c392255417e
Hironori Shiina <shiina.hironori@jp.fujitsu.com>
2018-02-14 21:10:26 +0900

    Build instance PXE options for unrescue
    This patch always builds PXE options for booting a user image even if
    the provision state is related to rescue mode. When the state is
    unrescuing, these options are necessary to boot a user image. When the
    provision state is rescuing, these options are just ignored because
    the PXE configuration is used at deployment mode.
    
    Change-Id: I1ff51cab120e6612c71e5d9c45cf28797bc16e76
    Closes-Bug: 1749433
    (cherry picked from commit 4df93fc4746184f42e852fbb372c0662aa829a55)

--- ironic/tests/unit/drivers/modules/test_pxe.py
+++ ironic/tests/unit/drivers/modules/test_pxe.py
@@ -249,11 +249,11 @@ class PXEPrivateMethodsTestCase(db_base.DbTestCase):
             'deployment_aki_path': pxe_kernel,
             'tftp_server': tftp_server,
             'ipxe_timeout': 0,
+            'ari_path': ramdisk,
+            'aki_path': kernel,
         }
 
-        if mode == 'deploy':
-            expected_options.update({'ari_path': ramdisk, 'aki_path': kernel})
-        elif mode == 'rescue':
+        if mode == 'rescue':
             self.node.provision_state = states.RESCUING
             self.node.save()
 
@@ -412,10 +412,11 @@ class PXEPrivateMethodsTestCase(db_base.DbTestCase):
             'deployment_aki_path': pxe_kernel,
             'tftp_server': tftp_server,
             'ipxe_timeout': ipxe_timeout_in_ms,
+            'ari_path': ramdisk,
+            'aki_path': kernel,
         }
-        if mode == 'deploy':
-            expected_options.update({'ari_path': ramdisk, 'aki_path': kernel})
-        elif mode == 'rescue':
+
+        if mode == 'rescue':
             self.node.provision_state = states.RESCUING
             self.node.save()
 
