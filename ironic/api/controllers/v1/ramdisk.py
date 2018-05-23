d6deb1ee4a2590d8a5e2032518adddf090e92f8d
Dao Cong Tien <tiendc@vn.fujitsu.com>
2018-05-23 10:40:31 +0700

    Missing import of "_"
    Function _() are used in these files but not imported.
    Eg. ramdisk.py: L178
        base_image_service.py: L198
    
    Change-Id: I5d424c59077193ae64e73a8fdb7f758fa3dc2714

--- ironic/api/controllers/v1/ramdisk.py
+++ ironic/api/controllers/v1/ramdisk.py
@@ -25,6 +25,7 @@ from ironic.api.controllers.v1 import types
 from ironic.api.controllers.v1 import utils as api_utils
 from ironic.api import expose
 from ironic.common import exception
+from ironic.common.i18n import _
 from ironic.common import policy
 from ironic.common import states
 from ironic.common import utils
