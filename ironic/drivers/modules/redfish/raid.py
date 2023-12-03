578c02813d983e21501a1b9136d57c30bc2b0daa
James Denton <james.denton@rackspace.com>
2023-12-03 06:10:28 -0600

    Fix log message var reference
    Fixes an issue with debug logging referencing node vs node_uuid.
    
    Change-Id: Ic7de9826fbec32038947be89b14f6dfdc2248de4

--- ironic/drivers/modules/redfish/raid.py
+++ ironic/drivers/modules/redfish/raid.py
@@ -626,7 +626,7 @@ def _construct_volume_payload(
         payload['Drives'] = [{"@odata.id": _drive_path(storage, d)} for d in
                              physical_disks]
     LOG.debug('Payload for RAID logical disk creation on node %(node_uuid)s: '
-              '%(payload)r', {'node': node.uuid, 'payload': payload})
+              '%(payload)r', {'node_uuid': node.uuid, 'payload': payload})
     return payload
 
 
