856238c56acb669c8e10cf1f0e0f4e0c9467c7e9
Steve Baker <sbaker@redhat.com>
2025-10-16 11:57:36 +1300

    Switch graphical console image to ubuntu by default
    The centos Containerfile still exists and the launch scripts have been
    adapted to work on both distros.
    
    The ubuntu container has been tested with noble. The container built
    in the CI jobs is bound to the version of ubuntu which the host is
    running, which will provide functional testing validation when jobs are
    moved to newer releases.
    
    Change-Id: I1954e418543acf939bf65189121484e038f3737c
    Signed-off-by: Steve Baker <sbaker@redhat.com>
    Signed-off-by: Julia Kreger <juliaashleykreger@gmail.com>

--- tools/vnc-container/bin/stop-firefox.sh
+++ tools/vnc-container/bin/stop-firefox.sh
@@ -5,7 +5,7 @@ set -ex
 connections=$(ss --no-header state established '( dport = :5900 or sport = :5900 )' | wc -l)
 
 if [ "$connections" -eq 0 ]; then
-    killall -s SIGTERM firefox
+    killall -s SIGTERM $FIREFOX
 else
-    echo "Active VNC connection detected, deferring firefox shutdown."
+    echo "Active VNC connection detected, deferring $FIREFOX shutdown."
 fi
